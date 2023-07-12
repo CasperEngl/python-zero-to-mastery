843996061912aa0bf2427906668715ee35977dc2
Casper Engelmann <cen@makeinfluence.com>
2023-07-12 21:04:06 +0200

    Add script to get user's name and age and print a formatted string using both concatenation and f-string methods

---
+++ 34. Formatted Strings/main1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello, your name is " + name + " and you are " + age + " years old.")
+
+print(f"Hello, your name is {name} and you are {age} years old.")
