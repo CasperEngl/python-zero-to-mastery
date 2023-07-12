691e8dad2681d474db08bf19aa6de00d94090213
Casper Engelmann <cen@makeinfluence.com>
2023-07-12 21:33:25 +0200

    Add examples of built-in functions and methods in Python

---
+++ 37. Built-In Functions + Methods/main.py
@@ -0,0 +1,19 @@
+print(len("hellloooooo"))
+
+greet = "helloooooo"
+
+print(greet[0:len(greet)])
+
+quote = "to be or not to be"
+
+print(quote.upper())
+
+print(quote.capitalize())
+
+print(quote.find("be"))
+
+print(quote.replace("be", "me"))
+
+print(quote)
+
+print(quote.replace("be", "me", 1))
