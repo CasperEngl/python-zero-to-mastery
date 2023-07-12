aeaa8dc535ef59618799037e14789e096087bdb4
Casper Engelmann <cen@makeinfluence.com>
2023-07-12 23:51:08 +0200

    Add dictionary methods to manipulate and access dictionary items

--- 55. Dictionary Methods 2/main.py
+++ 55. Dictionary Methods 2/main.py
@@ -1,1 +1,36 @@
 # 55. Dictionary Methods 2
+user = {
+    'basket': [1, 2, 3],
+    'greet': 'hello',
+    'age': 20
+}
+
+user_2 = user.copy()
+
+print('basket' in user.keys())
+
+print('hello' in user.values())
+
+print(user.items())
+
+user.clear()
+
+print(user)
+
+print(user_2)
+
+print(user_2.pop('age'))
+
+print(user_2)
+
+print(user_2.popitem())
+
+print(user_2)
+
+print(user_2.update({'age': 55}))
+
+print(user_2)
+
+print(user_2.update({'ages': 55}))
+
+print(user_2)
