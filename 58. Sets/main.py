d6a05cce3eb7dc3105ce6b15d0a8150374344920
Casper Engelmann <cen@makeinfluence.com>
2023-07-13 00:52:10 +0200

    Add code to demonstrate the use of sets and perform various operations on them

--- 58. Sets/main.py
+++ 58. Sets/main.py
@@ -1,1 +1,24 @@
 # 58. Sets
+
+my_set = {1, 2, 3, 4, 5, 5}
+
+my_set.add(100)
+my_set.add(2)
+
+print(my_set)
+
+my_list = [1, 2, 3, 4, 5, 5]
+
+print(set(my_list))
+
+print(1 in my_set)
+
+print(len(my_set))
+
+print(list(my_set))
+
+new_set = my_set.copy()
+my_set.clear()
+
+print(new_set)
+print(my_set)
