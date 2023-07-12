869697d168c9ea062fa758790d8ef59ec3315b85
Casper Engelmann <cen@makeinfluence.com>
2023-07-13 00:33:22 +0200

    Add code to demonstrate tuple indexing, slicing, count, index, and length

--- 57. Tuples 2/main.py
+++ 57. Tuples 2/main.py
@@ -1,1 +1,16 @@
 # 57. Tuples 2
+
+my_tuple = (1, 2, 3, 4, 5, 5)
+
+x = my_tuple[0]
+y = my_tuple[1]
+
+x, y, z, *other = my_tuple
+
+print(x, y, z, other)
+
+print(my_tuple.count(5))
+
+print(my_tuple.index(5))
+
+print(len(my_tuple))
