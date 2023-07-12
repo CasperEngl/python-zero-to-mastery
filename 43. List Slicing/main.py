96993e24688423987bc1389495bf6e8958b39b40
Casper Engelmann <cen@makeinfluence.com>
2023-07-12 22:07:45 +0200

    Add list slicing functionality to main.py and update the cart accordingly

---
+++ 43. List Slicing/main.py
@@ -0,0 +1,14 @@
+amazon_cart = ["notebooks", "sunglasses", "toys", "grapes"]
+
+amazon_cart[0] = "laptop"
+
+print(amazon_cart)
+
+new_cart = amazon_cart[0:3]
+
+print(new_cart)
+
+new_cart[0] = "gum"
+
+print(new_cart)
+print(amazon_cart)
