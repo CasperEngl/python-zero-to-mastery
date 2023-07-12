41e977f478c824da5db4e04d86d8a5f48aaf7047
Casper Engelmann <cen@makeinfluence.com>
2023-07-12 22:39:00 +0200

    Add code to demonstrate common list patterns and join strings in Python

---
+++ 48. Common List Patterns/main.py
@@ -0,0 +1,13 @@
+basket = ["a", "x", "b", "c", "d", "e", "d"]
+basket.sort()
+basket.reverse()
+print(basket[::-1])  # [::-1] reverses the list
+
+print(list(range(100)))
+
+sentence = " "
+sentence.join(["Hi", "my", "name", "is", "Casper"])
+print(sentence) # ''
+
+new_sentence = " ".join(["Hi", "my", "name", "is", "Casper"])
+print(new_sentence) # Hi my name is Casper
