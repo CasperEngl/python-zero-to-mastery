4582b79e940ebeffde197499567e06c1d9d21239
Casper Engelmann <cen@makeinfluence.com>
2023-07-12 22:27:42 +0200

    Add code to demonstrate various list methods such as index, in, and count

---
+++ 46. List Methods 2/main.py
@@ -0,0 +1,11 @@
+basket = [1, 2, 3, 4, 5]
+
+print(basket.index(5))
+
+basket_2 = ['a', 'b', 'c', 'd', 'e', 'd']
+
+print('d' in basket_2) # Check if 'd' is in the list
+
+print(basket_2.index('d', 4)) # Find the index of 'd' starting from index 4
+
+print(basket_2.count('d')) # Count the number of 'd's in the list
